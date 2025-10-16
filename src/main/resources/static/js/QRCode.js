35b1224d0c58f8ca5ceb5074e77b9d1f964c05a7
isaacHanna2 <isaac.hanna@dedalus.com>
2025-10-16 12:45:36 +0300

    send qr edit

--- src/main/resources/static/js/QRCode.js
+++ src/main/resources/static/js/QRCode.js
@@ -1,6 +1,7 @@
 function generateQR(QRcode,rowNum) {
-    const url = getBaseUrl(); 
-    const QRLink = `${url}/api/scanner/${QRcode}`;
+    const url = getBaseUrl(); // base url like test.wazna.us
+    
+    const QRLink = `${url}/scan/${QRcode}`;
     const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(QRLink)}`;
 
     // Create a hidden link element to trigger download
@@ -9,6 +10,7 @@ function generateQR(QRcode,rowNum) {
     link.download = `QR_${QRcode}.png`; // the file name for the downloaded image
     link.target = "_blank"; // optional: open image before download
 
+    
     // Trigger the download
     link.click();
 }
